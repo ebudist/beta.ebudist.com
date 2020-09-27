5ea8e626342fd195ce9d43bab71a2e638f7f57aa
ebudist <59221512+ebudist@users.noreply.github.com>
2020-09-27 19:21:33 +0700

    Coba-bikin-route-controller-view

---
+++ app/Controllers/Testaja.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Testaja extends BaseController
+{
+    public function index()
+    {
+        //	return view('welcome_message');
+        return view('test');
+    }
+
+    //--------------------------------------------------------------------
+
+}
---
+++ app/Views/test.php
@@ -0,0 +1,3 @@
+<?php
+
+echo ('Test Aja Dulu Gan');
